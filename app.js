9f8186842c18f3e81ec94c87ec2b7a18b631eb27
Joel Trinidad <jth_2929@hotmail.com>
2022-04-26 13:07:59 -0500

    getting a list of explorer's usernames in node

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const explorers = JSON.parse(rawdata);
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-console.log(explorersInNode.length)
+//console.log(explorersInNode.length)
+
+// Part3: Get the explorer's usernames in Node
+const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+console.log(usernamesInNode)
