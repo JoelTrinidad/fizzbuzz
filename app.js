7b641b2a71b0c6bb1e32a8674ca98174aff50ac5
Joel Trinidad <jth_2929@hotmail.com>
2022-04-26 21:29:04 -0500

    assignFizzAndBuzzTrick function created

--- app.js
+++ app.js
@@ -59,5 +59,19 @@ const assignFizzBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-console.log(explorersInNodeAndFizzBuzzTrick);
+//console.log(explorersInNodeAndFizzBuzzTrick);
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+
+const assignFizzAndBuzzTrick = function(explorer){
+    if(explorer.score%5 === 0 || explorer.score%3 === 0){
+        explorer.trick = "";
+        if(explorer.score%3 === 0) explorer.trick += "FIZZ";
+        if(explorer.score%5 === 0) explorer.trick += "BUZZ";
+        return explorer;
+    }else{
+        explorer.trick = explorer.score;
+        return explorer;
+    }
+};
+
+const explorersInNodeAndFizzBuzz = explorersInNode.map((explorer) => assignFizzAndBuzzTrick(explorer));
