e749637b76430523ed7e9b3b6c6849b07ea76b68
Joel Trinidad <jth_2929@hotmail.com>
2022-04-26 21:02:42 -0500

    gatting a new list of explorers with property trick added and the value assigned is BUZZ if score = 5

--- app.js
+++ app.js
@@ -29,4 +29,19 @@ const assignFizzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
-console.log(explorersInNodeAndFizzTrick);
+//console.log(explorersInNodeAndFizzTrick);
+
+// Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
+//
+const assignBuzzTrick = function(explorer){
+    if(explorer.score%5 === 0){
+        explorer.trick = "BUZZ";
+        return explorer;
+    }else{
+        explorer.trick = explorer.score;
+        return explorer;
+    }
+};
+
+const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
+console.log(explorersInNodeAndBuzzTrick);
