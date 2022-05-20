0a619f4a113e2d0591cf10583f67c6701e2fb05d
Joel Trinidad <jth_2929@hotmail.com>
2022-05-20 15:41:26 -0500

    render test expect changed

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -1,17 +1,14 @@
-const Reader = require('./../../../lib/utils/Reader')
+const Reader = require("./../../../lib/utils/Reader");
 
-describe('Test for Reader', () => {
-
-    test('Return information from a given path', () => {
-        const explorers = Reader.readJsonFile("explorers.json")
-        const firstExplorerExpected = {
-            "name": "Woopa1",
-            "githubUsername": "ajolonauta1",
-            "score": 1,
-            "mission": "node",
-            "stacks": ["javascript", "reasonML", "elm"]
-          }
-        expect(explorers).not.toBeNull();
-        expect(explorers[0]).toContain(firstExplorerExpected);
-    });
-})
+describe("Test for Reader", () => {
+  test("Return information from a given path", () => {
+    const explorers = Reader.readJsonFile("explorers.json");
+    expect(explorers).not.toBeNull();
+    expect(explorers).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({  name: "Woopa1" }),
+        expect.objectContaining({ name: "Woopa2" }),
+      ])
+    );
+  });
+});
